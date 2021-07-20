65ba3365e9dbb4a919b08e57b36c607a1f85bde0
Cris7260 <wy1150398003@163.com>
2021-07-20 16:32:12 +0800

    Added input and corresponding output

--- hello.cpp
+++ hello.cpp
@@ -2,6 +2,12 @@
 using namespace std;
 int main()
 {
-	cout<<"Hello, World!"<<endl;
+	cout<<"Hello World!"<<endl;
+	string instr;
+	cin>>instr;
+	if(instr=="Nokia")
+		cout<<"Hello Nokia!"<<endl;
+	else
+		cout<<"Oops! Unknown \""<<instr<<"\""<<endl;
 	return 0;
 }
